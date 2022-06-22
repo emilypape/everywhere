0118c0176a3555aa7b4fe491013c64cc96bf1fa5
emily <eap6787@gmail.com>
2022-06-21 18:29:33 -0600

    add datepicker

--- client/src/components/TravelForm/index.js
+++ client/src/components/TravelForm/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon } from '@iconify/react';
 import TravelFormCss from '../../assets/css/travelForm.css';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function TravelForm() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
   return (
     <div className='z-50 absolute shadow-2xl shadow-inner bg-white p-5 mt-3 rounded-lg'>
       <h1 className='font-bold text-3xl'>Find places to stay Everywhere</h1>
@@ -18,6 +23,24 @@ function TravelForm() {
               type='text'
               placeholder='Anywhere'
             />
+          </div>
+          <div className='flex border-lightgrey border-2 rounded-lg mt-3 justify-evenly'>
+            <div className='flex flex-col '>
+              <h1 className='font-bold text-sm ml-4 mt-1'>Check In</h1>
+              <DatePicker
+                className='multiselect__input ProseMirror p-3'
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+              />
+            </div>
+            <div>
+              <h1 className='font-bold text-sm ml-4 mt-1'>Check Out</h1>
+              <DatePicker
+                className='multiselect__input ProseMirror p-3'
+                selected={endDate}
+                onChange={(date) => setEndDate(date)}
+              />
+            </div>
           </div>
         </form>
       </div>
