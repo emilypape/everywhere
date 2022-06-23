b457a67e16e5dd4c06cd9598ff9fcfaf6951ceec
emily <eap6787@gmail.com>
2022-06-23 15:23:00 -0600

    implement react router dom;

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Homepage from './components/Homepage';
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Homepage />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
