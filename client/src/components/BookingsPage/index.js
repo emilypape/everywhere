e385f0af79d2f7b47595a7a497819107e1955741
emily <eap6787@gmail.com>
2022-06-29 12:26:07 -0600

    add post for favorites front end

--- client/src/components/BookingsPage/index.js
+++ client/src/components/BookingsPage/index.js
@@ -5,19 +5,34 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 import { Icon } from '@iconify/react';
 import { Link } from 'react-router-dom';
+import { ADD_FAVORITE } from '../../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 function BookingPage({ data }) {
+  const [addFavorites] = useMutation(ADD_FAVORITE);
   const [listings, setListings] = useState(data);
   const [favorited, setFavorited] = useState();
   const location = useLocation();
   if (!location.state) {
     return <div>Sorry, no bookings here, go back!</div>;
   }
+  // import destructured data from homepage using location.state
   const { adults, children, startDate, endDate, locations } = location.state;
 
+  // put date object in readable format
   const displayStartDate = startDate.toString().slice(4, 11);
   const displayEndDate = endDate.toString().slice(4, 11);
 
+  const postFavorite = async (e) => {
+    e.preventDefault();
+    try {
+      await addFavorites({ variables: e.target.id });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // filter listings
   const filteredListings = listings.filter(function (listing) {
     return listing.address.includes(locations) && listing.availability.length && listing.rooms >= adults;
   });
@@ -29,7 +44,14 @@ function BookingPage({ data }) {
           return (
             <div className='flex flex-col mb-10'>
               <div className='flex justify-end heart mr-3'>
-                <Icon icon='ant-design:heart-twotone' color={`white`} width='26' height='26' />
+                <Icon
+                  id={listing.id}
+                  icon='ant-design:heart-twotone'
+                  color={`white`}
+                  width='26'
+                  height='26'
+                  onClick={postFavorite}
+                />
               </div>
 
               <Carousel showStatus={false} width={300} showThumbs={false} className='custom-slider rounded-lg carousel'>
