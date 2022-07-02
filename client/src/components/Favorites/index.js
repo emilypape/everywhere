df1b449a880a76ea1d0ddfeb753e3d748578fad4
emily <eap6787@gmail.com>
2022-07-02 17:34:17 -0600

    fix favorite styles

--- client/src/components/Favorites/index.js
+++ client/src/components/Favorites/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_FAVORITES } from '../../utils/queries';
 import { useSelector, useDispatch } from 'react-redux';
+import { Carousel } from 'react-responsive-carousel';
 
 function Favorites() {
   const dispatch = useDispatch();
@@ -27,24 +28,32 @@ function Favorites() {
   }
 
   return (
-    <div className='I-M-Here'>
-      <h2>ID's!</h2>
+    <div className='I-M-Here flex justify-evenly mt-10 flex-wrap mb-64'>
       {favorites &&
         favorites[0].map((favorite) => (
-          <p key={favorite.favoriteId}>
-            favoriteID: {favorite.favoriteId}
-            <br />
-            favorite Title: {favorite.favoriteTitle}
-            <br />
-            favorite image: {favorite.favoritePhoto}
-            <br />
-            favorite price: {favorite.favoritePrice}
-            <br />
-            <br />
-          </p>
+          <div>
+            <Carousel showStatus={false} width={300} showThumbs={false} className='custom-slider rounded-lg carousel'>
+              <img className='imageSizes' src={favorite.favoritePhoto} />
+            </Carousel>
+            <div className='font-semibold mt-2'>{favorite.favoriteTitle}</div>
+            <div className='font-bold mb-5'>${favorite.favoritePrice} night</div>
+          </div>
         ))}
     </div>
   );
 }
 
 export default Favorites;
+{
+  /* <p key={favorite.favoriteId}>
+favoriteID: {favorite.favoriteId}
+<br />
+favorite Title: {favorite.favoriteTitle}
+<br />
+favorite image: {favorite.favoritePhoto}
+<br />
+favorite price: {favorite.favoritePrice}
+<br />
+<br />
+</p> */
+}
